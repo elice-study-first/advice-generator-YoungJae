bc51129e8152b66b8d92d423e951cc2e7b5e2a0b
young <rhsiddlsidhd@nate.com>
2023-10-10 14:59:34 +0900

    docs: '폴더  변경' 기존 base code 를 상위 폴더 내 template 폴더로 이동

---
+++ miniproject/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Advice #</div>
+        <div>""</div>
+        <button>+</button>
+        <button>=</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
