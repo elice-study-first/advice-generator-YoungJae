d376728e0c5e6237a92ee8a1c4ed27caef546952
young <rhsiddlsidhd@nate.com>
2023-10-12 16:18:00 +0900

    🎨 Button을 이용한 Advice 출력

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [state, setState] = useState("");
+  const [rolling, setRolling] = useState(false);
+
+  const getData = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice").then((res) =>
+      res.json()
+    );
+    const randomData = res.slip.advice;
+
+    setState(randomData);
+  };
+
+  useEffect(() => {
+    if (rolling) {
+      const interval = setInterval(() => {
+        getData();
+      }, 500);
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [rolling]);
+
+  const rollingRun = () => {
+    setRolling(true);
+  };
+
+  const rollingStop = () => {
+    setRolling(false);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <div>Advice #</div>
-        <div>""</div>
-        <button>+</button>
-        <button>=</button>
+        <div>A D V I C E # 1 1 7</div>
+        <div className="Random_Advice">{state}</div>
+        <div className="Under_Bar"></div>
+        <button className="Stop_Btn" onClick={rollingStop}>
+          <i className="fa-solid fa-pause"></i>
+        </button>
+        <button className="Run_Btn" onClick={rollingRun}>
+          <i className="fa-solid fa-dice-five"></i>
+        </button>
       </header>
     </div>
   );
